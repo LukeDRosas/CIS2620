0baa49e12505ca545d11725f3c88342ca65d8222
LukeDRosas <33234746+LukeDRosas@users.noreply.github.com>
2018-01-19 14:39:42 -0600

    Add files via upload

---
+++ GreenvilleMotto.cs
@@ -0,0 +1,11 @@
+//Filename GreenvilleMotto
+//Written by Luke Doughty-Rosas
+//Written on 1/18/18
+using static System.Console;
+class GreenvilleMotto
+{
+   static void Main()
+   {
+      WriteLine("The stars shine in Greenville.");
+   }
+}
---
+++ GreenvilleMotto2.cs
@@ -0,0 +1,13 @@
+//Filename GreenvilleMotto2
+//Written by Luke Doughty-Rosas
+//Written on 1/18/18
+using static System.Console;
+class GreenvilleMotto2
+{
+   static void Main()
+   {
+	  WriteLine("********************************"); 
+      WriteLine("*The stars shine in Greenville.*");
+	  WriteLine("********************************");
+   }
+}
